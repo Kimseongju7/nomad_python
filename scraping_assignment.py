289950145a0112822ddfd9b3fdd8b69816f7b93c
Kimseongju7 <rlatj777@naver.com>
2024-11-11 00:25:19 +0900

    에러 수정

--- scraping_assignment.py
+++ scraping_assignment.py
@@ -14,14 +14,14 @@ class Job:
         return f"company : {self.company}, title : {self.title}\ndescription : {self.des}\nurl : {self.url}"
 
 def web_scrapper(url):
-    response = requests.get(url, headers={"User-Agent":"o"})
+    response = requests.get(url, headers={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"})
     soup = BeautifulSoup(response.content, "html.parser")
     pages = len(soup.find("ul", class_="bsj-nav").find_all("a"))
     if not pages:
         pages = 1
     for page in range(pages):
         tmp_url = f"{url}page/{page+1}/"
-         resp = requests.get(tmp_url, headers={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"})
+        resp = requests.get(tmp_url, headers={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"})
         soup = BeautifulSoup(resp.content, "html.parser")
         jobs = soup.find_all('li', class_="bjs-jlid")
         for job in jobs:
@@ -37,16 +37,10 @@ def web_scrapper(url):
                 "description": description,
                 "url": link
             })
-        
+
 skils = ['python', 'typescript', 'javascript']
 urls = ['https://berlinstartupjobs.com/engineering/']
 for skil in skils:
     urls.append(f"https://berlinstartupjobs.com/skill-areas/{skil}/")
 for url in urls:
     web_scrapper(url=url)
-
-file = open("jobs.csv", mode = "w")
-writer = csv.writer(file)
-writer.writerow(["company", "title", "description", "url"])
-for job in job_datas:
-    writer.writerow(job.values())
